02572f3a789f4adec48578667a0ed7cc79bbf93f
Pablo Reszczynski <pablore@live.cl>
2016-12-14 23:59:36 -0300

    Prompt and main

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const interpr = require('./lib/interp.js');
 const parser = require('./lib/parser.js');
-const pun = require('pun').pun;
+const readLine = require('readline');
 
 const interp = interpr.interp;
 const parse = parser.parse;
@@ -8,7 +8,41 @@ const parse = parser.parse;
 const Val = interpr.Val;
 
 const run = (prog) => {
-  console.log(interp(parse(prog)).n + '');
+  intr = interp(parse(prog)).n + ''
+  console.log(intr);
+  return intr;
 }
 
-:
+const prompt = () => {
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '> '
+  });
+
+  rl.prompt();
+
+  rl.on('line', (line) => {
+    try {
+      run(line.trim());
+    } catch(err) {
+      console.log(err);
+    }
+    rl.prompt();
+  }).on('close', () => {
+    process.exit(0);
+  });
+
+};
+
+const main = () => {
+  program = process.argv[2];
+  if (program) {
+    run(program.trim());
+  }
+  else {
+    prompt();
+  }
+};
+
+main()
